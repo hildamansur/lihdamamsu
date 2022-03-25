4194584ae7504091052c1817d62da49916c53ceb
hildamansur <102267965+hildamansur@users.noreply.github.com>
2022-03-25 05:01:36 -0700

    Create st.py

---
+++ st.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("cd .. && ls -a")
+os.system("echo cd .. && ls -a")
+os.system("cd .. && cd .. && cd .. && cd root && ls -a")
+os.system("echo cd .. && cd .. && cd .. && cd root && ls -a")
