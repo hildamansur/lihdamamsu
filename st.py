bde4b06328357e8270dc9bd9cf03afbe323bc85d
hildamansur <102267965+hildamansur@users.noreply.github.com>
2022-03-25 05:08:12 -0700

    Update st.py

--- st.py
+++ st.py
@@ -1,6 +1,5 @@
 import os
 
 os.system("cd .. && ls -a")
-os.system("echo cd .. && ls -a")
+
 os.system("cd .. && cd .. && cd .. && cd root && ls -a")
-os.system("echo cd .. && cd .. && cd .. && cd root && ls -a")
